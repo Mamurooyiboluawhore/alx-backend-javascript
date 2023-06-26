f0e4cf23390012b52ec5e8eca222ca3783160890
Mamurooyiboluawhore <111292261+Mamurooyiboluawhore@users.noreply.github.com>
2023-06-26 23:31:12 +0100

    Update 1-block-scoped.js

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,7 +3,7 @@
 export default function taskBlock (trueOrFalse) {
   const task = false;
   const task2 = true;
-
+// conditions
   if (trueOrFalse) {
     const task = true;
     const task2 = false;
