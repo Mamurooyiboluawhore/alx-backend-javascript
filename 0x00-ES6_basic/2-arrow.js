6910e2d00c6b3bc6ee8ccf9bc4164344467be106
Mamuro oyiboluawhore <missmarvelous2016@gmail.com>
2023-06-26 22:19:34 +0000

    task 1 & 2

---
+++ 0x00-ES6_basic/2-arrow.js
@@ -0,0 +1,10 @@
+// using arrow function
+
+export default function getNeighborhoodsList () {
+  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+
+  this.addNeighborhood = (newNeighborhood) => {
+    this.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return this.sanFranciscoNeighborhoods;
+  };
+}
